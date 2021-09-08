719e2b5d075d478610fd6378e384c9bc14f965d0
Crowdbotics <team@crowdbotics.com>
2021-09-08 22:16:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "dawn_tooth_30403Identifier",
+  url: "https://dawn_tooth_30403Identifier.botics.co",
+  api: "https://dawn_tooth_30403Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
